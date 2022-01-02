ce5ccbd41f5527bf0f5a299fe7009bd46bcdf69c
ul6x06 <91857915+ul6x06@users.noreply.github.com>
2022-01-03 03:49:44 +0800

    Create Class04.java

---
+++ Class04.java
@@ -0,0 +1,17 @@
+package Java14;
+
+import java.io.*;
+
+public class Class04 {
+	public static void main(String args[]) throws IOException {
+		char data[] = new char[128];
+		FileReader fr = new FileReader("C:\\Users\\USER\\Documents\\donkey.txt");
+
+		int num = fr.read(data);
+		String str = new String(data, 0, num);
+		System.out.println("Characters read= " + num);
+		System.out.println(str);
+
+		fr.close();
+	}
+}
